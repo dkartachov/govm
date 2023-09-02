8c59afea2ebd84500576f41fa7a964b93ba1e521
Denis Kartachov <kartachovd@gmail.com>
2023-09-02 00:51:06 -0400

    move remote list to separate command

--- cmd/list.go
+++ cmd/list.go
@@ -8,30 +8,18 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
-	"github.com/dkartachov/govm/pkg/semver"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List versions of Go",
-	Long: `List all locally installed versions of Go by default.
-Specify --remote flag to list available versions to download.`,
+	Use:     "list",
+	Short:   "List versions of Go",
+	Long:    `List all locally installed versions of Go.`,
 	Aliases: []string{"ls"},
 	Run: func(cmd *cobra.Command, args []string) {
-		remote, _ := cmd.Flags().GetBool("remote")
-
-		if remote {
-			listRemote()
-		} else {
-			list()
-		}
+		list()
 	},
 }
 
@@ -48,54 +36,6 @@ func list() {
 	}
 }
 
-func listRemote() {
-	versions := availableVersions()
-	err := semver.Sort(versions, semver.Asc)
-
-	if err != nil {
-		log.Fatalf("error sorting versions: %v", err)
-	}
-
-	for _, v := range versions {
-		fmt.Println(v)
-	}
-}
-
-func availableVersions() []string {
-	r := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
-		URLs: []string{"https://github.com/golang/go"},
-	})
-
-	err := r.Fetch(&git.FetchOptions{})
-
-	if err != nil && err != git.NoErrAlreadyUpToDate {
-		log.Fatalf("error fetching references from remote repo: %v", err)
-	}
-
-	rfs, err := r.List(&git.ListOptions{})
-
-	if err != nil {
-		log.Fatalf("error retrieving list of remote references: %v", err)
-	}
-
-	var versions = []string{}
-
-	for _, ref := range rfs {
-		if ref.Name().IsTag() {
-			versionP := ref.Name().Short()
-
-			if semver.ValidP(versionP, "go") {
-				version := strings.TrimPrefix(versionP, "go")
-				versions = append(versions, version)
-			}
-		}
-	}
-
-	return versions
-}
-
 func init() {
 	rootCmd.AddCommand(listCmd)
-	// CHECKME should this be a separate command instead?
-	listCmd.Flags().Bool("remote", false, "list available versions to download")
 }
---
+++ cmd/listRemote.go
@@ -0,0 +1,83 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/dkartachov/govm/pkg/semver"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/storage/memory"
+	"github.com/spf13/cobra"
+)
+
+// listRemoteCmd represents the listRemote command
+var listRemoteCmd = &cobra.Command{
+	Use:     "list-remote",
+	Short:   "List versions available to download",
+	Long:    `List versions available to download`,
+	Aliases: []string{"lsr"},
+	Run: func(cmd *cobra.Command, args []string) {
+		limit, _ := cmd.Flags().GetInt("limit")
+		listRemote(limit)
+	},
+}
+
+func listRemote(limit int) {
+	versions := availableVersions()
+	err := semver.Sort(versions, semver.Asc)
+
+	if limit > 0 {
+		versions = versions[len(versions)-limit:]
+	}
+
+	if err != nil {
+		log.Fatalf("error sorting versions: %v", err)
+	}
+
+	for _, v := range versions {
+		fmt.Println(v)
+	}
+}
+
+func availableVersions() []string {
+	r := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
+		URLs: []string{"https://github.com/golang/go"},
+	})
+
+	err := r.Fetch(&git.FetchOptions{})
+
+	if err != nil && err != git.NoErrAlreadyUpToDate {
+		log.Fatalf("error fetching references from remote repo: %v", err)
+	}
+
+	rfs, err := r.List(&git.ListOptions{})
+
+	if err != nil {
+		log.Fatalf("error retrieving list of remote references: %v", err)
+	}
+
+	var versions = []string{}
+
+	for _, ref := range rfs {
+		if ref.Name().IsTag() {
+			versionP := ref.Name().Short()
+
+			if semver.ValidP(versionP, "go") {
+				version := strings.TrimPrefix(versionP, "go")
+				versions = append(versions, version)
+			}
+		}
+	}
+
+	return versions
+}
+
+func init() {
+	rootCmd.AddCommand(listRemoteCmd)
+	listRemoteCmd.Flags().Int("limit", 0, "limit the versions returned. Default 0 (no limit)")
+}
